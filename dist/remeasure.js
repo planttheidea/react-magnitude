9deb13a8a84e394276c1ec936decaf2d1d1cf6e8
planttheidea <tony.quetano@planttheidea.com>
2016-07-22 08:05:22 -0400

    Remove unncessary typeof checker

--- dist/remeasure.js
+++ dist/remeasure.js
@@ -100,11 +100,9 @@ var Remeasure =
 	 * window object
 	 */
 	var setRaf = function setRaf() {
-	  if (typeof window !== 'undefined') {
-	    raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
-	      window.setTimeout(callback, 1000 / 60);
-	    };
-	  }
+	  raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+	    window.setTimeout(callback, 1000 / 60);
+	  };
 	};
 	
 	/**
--- src/index.js
+++ src/index.js
@@ -34,16 +34,14 @@ let raf;
  * window object
  */
 const setRaf = () => {
-  if (typeof window !== 'undefined') {
-    raf = (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-      }
-    );
-  }
+  raf = (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
 };
 
 /**
