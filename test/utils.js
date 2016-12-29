3a793acd8d465b5473351d67f5033cecad220f1f
planttheidea <tony.quetano@planttheidea.com>
2016-12-29 00:55:55 -0500

    V2 updates... * Split out some of the logic to be more functional * Remove a lot of shallow object cloning to leverage reduce based on keys instead * Shrink the size of state to be based on the specific keys requested instead of all keys * Revert measurements to 0 when the element does not exist

--- test/utils.js
+++ test/utils.js
@@ -1,29 +1,43 @@
 import test from 'ava';
 
+import _ from 'lodash';
+
 import {
-  createObjectFromKeys,
+  createIsKeyType,
+  getKeyType,
+  getKeysSubsetWithType,
   getNaturalDimensionValue,
+  getRequestAnimationFrame,
+  getScopedValues,
   getValidKeys,
-  getValues,
+  haveValuesChanged,
+  isElementVoidTag,
+  isPositionKey,
+  isSizeKey,
+  reduceStateToMatchingKeys
 } from '../src/utils';
 
-const DEFAULT_OPTIONS = {
-  positionProp: 'position',
-  renderOnResize: true,
-  sizeProp: 'size'
+import {
+  ALL_POSITION_KEYS,
+  ALL_SIZE_KEYS
+} from '../src/constants';
+
+const sleep = (ms = 0) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
 };
 
-test('if createObjectFromKeys creates an object from the keys passed', (t) => {
-  const keys = ['foo', 'bar'];
-  const source = {
-    foo: 'bar',
-    bar: 'baz'
-  };
-  const target = createObjectFromKeys(keys, source);
+test('if createIsKeyType creates a function', (t) => {
+  const result = createIsKeyType(['foo']);
 
-  t.not(source, target);
-  t.deepEqual(source, target);
+  t.true(_.isFunction(result));
 });
+
+test.todo('getKeyType');
+test.todo('getKeysSubsetWithType');
 
 test('if getNaturalDimensionValue gets the correct value based on key', (t) => {
   const objectWithoutNaturalValue = {
@@ -38,17 +52,46 @@ test('if getNaturalDimensionValue gets the correct value based on key', (t) => {
   t.is(getNaturalDimensionValue(objectWithNaturalValue, 'naturalHeight'), objectWithNaturalValue.naturalHeight);
 });
 
-test('if getValidKeys correctly limits the keys returned', (t) => {
-  const keys = ['foo', 'bar', 'baz'];
-  const keysToTestAgainst = ['foo', 'baz', 'blah', 'dee'];
-  const validKeys = getValidKeys(keys, keysToTestAgainst);
+test('if getRequestAnimationFrame gets requestAnimationFrame from the global object', async (t) => {
+  const result = getRequestAnimationFrame();
 
-  t.deepEqual(validKeys, ['foo', 'baz']);
+  t.true(_.isFunction(result));
+
+  let hasRun = false;
+
+  result(() => {
+    hasRun = true;
+  });
+
+  t.false(hasRun);
+
+  await sleep(1000 / 60);
+
+  t.true(hasRun);
 });
 
-test('if getValues returns an object with size or position or both with the correct values', (t) => {
-  const sizeKeys = ['height', 'offsetHeight'];
-  const positionKeys = ['left', 'offsetLeft'];
+test('if getScopedValues returns an object with size or position or both with the correct values', (t) => {
+  const sizeKeys = [
+    {
+      key: 'height',
+      source: 'clientRect',
+      type: 'size'
+    }, {
+      key: 'offsetHeight',
+      source: 'element',
+      type: 'size'
+    }
+  ];
+  const positionKeys = [
+    {
+      key: 'left',
+      source: 'clientRect',
+      type: 'position'
+    }, {
+      key: 'offsetLeft',
+      source: 'clientRect',
+      type: 'position'
+    }];
   const allKeys = [
     ...sizeKeys,
     ...positionKeys
@@ -76,11 +119,60 @@ test('if getValues returns an object with size or position or both with the corr
     ...positionResult
   };
 
-  const sizeValues = getValues(sizeKeys, currentState, DEFAULT_OPTIONS);
-  const positionValues = getValues(positionKeys, currentState, DEFAULT_OPTIONS);
-  const allValues = getValues(allKeys, currentState, DEFAULT_OPTIONS);
+  const sizeValues = getScopedValues(sizeKeys, currentState, false);
+  const positionValues = getScopedValues(positionKeys, currentState, false);
+  const allValues = getScopedValues(allKeys, currentState, false);
 
   t.deepEqual(sizeValues, sizeResult);
   t.deepEqual(positionValues, positionResult);
   t.deepEqual(allValues, allResult);
 });
+
+test('if getValidKeys correctly limits the keys returned', (t) => {
+  const keys = ['foo', 'bar', 'baz'];
+  const keysToTestAgainst = ['foo', 'baz', 'blah', 'dee'];
+  const validKeys = getValidKeys(keys, keysToTestAgainst);
+
+  t.deepEqual(validKeys, ['foo', 'baz']);
+});
+
+test('if haveValuesChanged checks for changes between the two objects', (t) => {
+  const keys = [{
+    key: 'foo'
+  }];
+  const values = {
+    foo: 'bar'
+  };
+  const sameValues = {
+    foo: 'bar'
+  };
+  const changedValues = {
+    foo: 'baz'
+  };
+
+  t.false(haveValuesChanged(keys, values, sameValues));
+  t.true(haveValuesChanged(keys, values, changedValues));
+});
+
+test.todo('isElementVoidTag');
+
+test('if isPositionKey determines if the key is a position property', (t) => {
+  ALL_POSITION_KEYS.forEach((key) => {
+    t.true(isPositionKey(key));
+  });
+
+  ALL_SIZE_KEYS.forEach((key) => {
+    t.false(isPositionKey(key));
+  });
+});
+
+test('if isSizeKey determines if the key is a size property', (t) => {
+  ALL_POSITION_KEYS.forEach((key) => {
+    t.false(isSizeKey(key));
+  });
+
+  ALL_SIZE_KEYS.forEach((key) => {
+    t.true(isSizeKey(key));
+  });
+});
+test.todo('reduceStateToMatchingKeys');
